0323bf454b2fb594780abe17034282a778cddf8f
Jeffrey Chen <jeffreychen2016@gmail.com>
2018-12-22 14:13:29 -0600

    finished

--- LeapYear/Program.cs
+++ LeapYear/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LeapYear
 {
@@ -6,7 +7,32 @@ namespace LeapYear
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var leap = false;
+            var input = new List<int> { 1800, 1900, 2100, 2200, 2300 , 2500, 2000, 2400, 1980 };
+
+            foreach (var i in input)
+            {
+                if (i % 400 == 0)
+                {
+                    leap = true;
+                    Console.WriteLine($"{i}:{leap}");
+                }
+                else if (i % 100 == 0)
+                {
+                    leap = false;
+                    Console.WriteLine($"{i}:{leap}");
+                }
+                else if (i / 4 == 0)
+                {
+                    leap = true;
+                    Console.WriteLine($"{i}:{leap}");
+                }
+                else
+                {
+                    leap = false;
+                    Console.WriteLine($"{i}:{leap}");
+                }
+            }
         }
     }
 }
